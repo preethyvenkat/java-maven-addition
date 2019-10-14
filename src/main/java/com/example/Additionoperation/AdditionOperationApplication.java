b99dc171dc682986328b3eafeae85d71d36fda32
Raja Sundaram <sundarsy@gmail.com>
2019-10-14 15:52:51 +0800

    added source code

---
+++ src/main/java/com/example/Additionoperation/AdditionOperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.Additionoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionOperationApplication.class, args);
+	}
+
+}
